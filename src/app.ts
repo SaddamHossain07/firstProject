a602ae0fb29be5614df0560b371a2dd28cbcd86a
SaddamHossain07 <contact.saddam3@gmail.com>
2024-05-16 05:21:02 +0600

    complete setup and first model created

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { StudentRoutes } from './app/modules/student/student.route';
 
 const app: Application = express();
 
@@ -7,12 +8,13 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
-  let b = 30;
+// application routes
+app.use('/api/v1/students', StudentRoutes);
 
-  const a = 10;
+const getController = (req: Request, res: Response) => {
+  res.send('hello dev');
+};
 
-  res.send(a);
-});
+app.get('/', getController);
 
 export default app;
---
+++ src/app/modules/student/student.controller.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from 'express';
+import { StudentServices } from './student.service';
+
+const createStudent = async (req: Request, res: Response) => {
+  try {
+    const { student: studentData } = req.body;
+    console.log(studentData);
+    // will call service func to send data
+    const result = await StudentServices.createStudentIntoDb(studentData);
+    // sent responst
+    res.status(200).json({
+      success: true,
+      message: 'Student is created successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getAllStudent = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentFromDb();
+    res.status(200).json({
+      success: true,
+      message: 'Students are retrieved successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getSingleStudent = async (req: Request, res: Response) => {
+  try {
+    const { studentId } = req.params;
+    const result = await StudentServices.getSingleStudentFromDb(studentId);
+    res.status(200).json({
+      success: true,
+      message: 'A Single Student is retrieved successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const StudentController = {
+  createStudent,
+  getAllStudent,
+  getSingleStudent,
+};
